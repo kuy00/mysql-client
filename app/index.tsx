26a04036926b70939e58965a346d68566349207d
kuy <kuy472013@gmail.com>
2025-01-05 14:49:38 +0900

    init project

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
