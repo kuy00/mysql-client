4949978fd1e4b32a84c721832ab6118d3b750499
kuy <kuy472013@gmail.com>
2025-01-11 22:57:37 +0900

    root layout 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { useColorSchemeTheme } from "@/hooks/useColorSchemeTheme";
 import { ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useMigration } from "@/hooks/useMigration";
 import { SQLiteProvider } from "expo-sqlite";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -17,15 +16,7 @@ const RootLayout = () => {
         onInit={useMigration}
       >
         <QueryClientProvider client={queryClient}>
-          <SafeAreaView
-            style={{ flex: 1, backgroundColor: theme.colors.background }}
-          >
-            <Stack
-              screenOptions={{
-                headerShown: false,
-              }}
-            />
-          </SafeAreaView>
+          <Stack screenOptions={{ headerTitle: "Mysql Client" }} />
         </QueryClientProvider>
       </SQLiteProvider>
     </ThemeProvider>
