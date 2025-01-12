788d014ae8344cca25bbe591ecad93fbadbb0f6a
kuy <kuy472013@gmail.com>
2025-01-12 17:49:39 +0900

    database 설정 변경

--- db/database.ts
+++ db/database.ts
@@ -2,7 +2,9 @@ import * as SQLite from "expo-sqlite";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import * as schemas from "@/db/schemas";
 
-const sqlite = SQLite.openDatabaseSync(process.env.EXPO_PUBLIC_DB_NAME);
+const sqlite = SQLite.openDatabaseSync(process.env.EXPO_PUBLIC_DB_NAME, {
+  useNewConnection: true,
+});
 
 const db = drizzle(sqlite, { schema: schemas });
 
