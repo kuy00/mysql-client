fd7f3c33ca5552aa92996c98f55ca21938a61109
kuy <kuy472013@gmail.com>
2025-02-02 19:43:04 +0900

    babel-plugin-transform-typescript-metadata 추가

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["inline-import", { extensions: [".sql"] }]],
+    plugins: [
+      ["inline-import", { extensions: [".sql"] }],
+      "babel-plugin-transform-typescript-metadata",
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ],
   };
 };
