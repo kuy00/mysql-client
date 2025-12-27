f39ded1c5189e13935a30d153c3eade0c9a266c1
kim wooyeong <kuy472013@gmail.com>
2025-12-27 16:49:04 +0900

    app package 변경

--- android/app/src/main/java/com/MysqlClient/MainActivity.kt
+++ android/app/src/main/java/com/MysqlClient/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.MysqlClient
+package com.MysqlClient
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/MysqlClient/MainApplication.kt
+++ android/app/src/main/java/com/MysqlClient/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.MysqlClient
+package com.MysqlClient
 
 import android.app.Application
 import android.content.res.Configuration
